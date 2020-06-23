27b32594f079458847e876df95808d773103ae96
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2020-06-23 17:20:32 -0500

    Un archivo unico para controlarlo a todos

---
+++ proyecto/s-00-main.sql
@@ -0,0 +1,8 @@
+--@Autor(es):       Paul Aguilar, Ana Laura Pérez Bueno
+--@Fecha creación:  24/06/2020
+--@Descripción:     Script para ejecutar todo
+
+@@s-01-usuarios.sql
+@@s-02-entidades.sql
+@@s-03-tablas-temporales.sql
+@@s-04-tablas-externas.sql
